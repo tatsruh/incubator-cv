3d9925d6bcf79ded77f9845f134ac0e87ce8cb55
tatsruh <tatsruh@gmail.com>
2025-09-24 14:14:01 +0200

    klk

--- src/layout/sections/aboutMe/aboutMe.tsx
+++ src/layout/sections/aboutMe/aboutMe.tsx
@@ -14,7 +14,7 @@ export const AboutMe = () => {
           <Text>Hi, my name is Tatiana, i am a Frontend Web Developper. My stack includes JavaScript, TypeScript,
           React, React-Router, Redux, Redux ToolKit, React Hook Form, REST API, Styled Components, Taliwind, Jira, Postman, Unit-Tests, Storybook and other tools</Text>
           <FlexWrapper>
-              <Number>1+</Number>
+              <Number>3+</Number>
               <NumberText>Year of experience. Specialised in building apps, while ensuring a seamless
                   web experience for end users.</NumberText>
           </FlexWrapper>
